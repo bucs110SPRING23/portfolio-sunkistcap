fec24c52d4d60222f90e26b45ad74f55695c7c7d
sunkistcap <WICKxDGMZ@gmail.com>
2023-02-15 17:30:25 -0500

    in class work

---
+++ ch03/exercises/examples.py
@@ -0,0 +1,25 @@
+#bool - boolean type
+#True/false
+# - caps are important
+# True,1, "Hello"
+print(type(True))
+print(bool(1),bool(-1), bool ("Hello"))
+#example 2
+x=5
+y=10
+print(x==y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+#exmaple 3
+print (True and True)
+print(True and False)
+print(False and True)
+print(False and True)
+print(False and False)
+
+print (True or True)
+print (True or False)
+print (False or True)
+print (False or False)
