09ec7510dd1badecd6335fba6c2ff92b1de28965
sunkistcap <WICKxDGMZ@gmail.com>
2023-03-07 16:19:22 -0500

    lab 4 and excersize

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -2,19 +2,39 @@
 import turtle
 import random
 #initalization
-player=turtle.Turtle()
+ranTurt=turtle.Turtle()
 screen=turtle.Screen()
-player.shape("turtle")
-player.color("purple")
-player.penup()
-player.goto(500,500)
-player.pendown()
-
-while (player.isvisible()==True):
+ranTurt.shape("turtle")
+ranTurt.color("purple")
+size=(turtle.screensize())
+print(size)
+height=(size[0])
+width=(size[1])
+nHeight=(height-(height*2))
+nWidth=(width-(width*2))
+#while loop
+while (ranTurt.isvisible()==True):
     coin=random.randint(1,100)
+    #heads
     if (coin>=50):
-        player.left(90)
-        player.fd(50)
-    if(coin<=50):
-        player.right(90)
-        player.fd(50)
+        ranTurt.left(90)
+        ranTurt.fd(50)
+    #tails
+    elif(coin<=50):
+        ranTurt.right(90)
+        ranTurt.fd(50)
+    #turtle location check
+    turtCheck=(ranTurt.pos())
+    print(turtCheck)
+    x=(int(turtCheck[0]))
+    y=(int(turtCheck[1]))
+    if(x>=width):
+        ranTurt.ht()
+    elif(x<=nWidth):
+        ranTurt.ht()
+    elif(y>=height ):
+        ranTurt.ht() 
+    elif(y<=nHeight):
+        ranTurt.ht()
+#exit
+turtle.bye()
