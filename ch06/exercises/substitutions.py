464f138a29f170969ca3c2a0a429a547ec878fd8
sunkistcap <WICKxDGMZ@gmail.com>
2023-04-16 05:45:46 -0400

    chapter 6 and chapter 7 lab

--- ch06/exercises/substitutions.py
+++ ch06/exercises/substitutions.py
@@ -1,13 +1,12 @@
 import json
 
-file= open('news.txt', 'r')
-sub=open("subs.json",'r')
-rewrite= open('betternews.txt', 'w')
-article = file.read()
-subs = json.load(sub)
-for key in subs:
-    article = article.replace(key, subs[key])
-rewrite.write(article)
-file.close()
-rewrite.close()
-sub.close()
+with open ('subs.json','r') as sub_file, open('news.txt','r',encoding='utf-8') as old_file:
+    article = old_file.read()
+    subs=json.load(sub_file)
+    for old_word,new_word in subs.items():
+        article= article.replace(old_word, new_word)
+    with open ('betternews.txt',"w") as new_file:
+        new_file.write(article)
+sub_file.close()
+old_file.close()
+new_file.close()
